813b39316c582aeca98f75d97864de81768b0fd6
juliaczernek <czernek.julka@gmail.com>
2025-12-16 17:50:14 +0100

    changing the lady sprouts RZIZON

--- routes/ladySprout.js
+++ routes/ladySprout.js
@@ -8,10 +8,13 @@ router.get('/', function(req, res, next) {
   // first we get the data
   const data = fs.readFileSync('public/databases/ladySprout.json', 'utf8');
   const questions = JSON.parse(data);
-  const firstQuestion = questions.questions[0]; 
-  console.log("First question:", firstQuestion);
+  const question1 = questions.questions[1]; 
+  const question2 = questions.questions[1]; 
+  const question3 = questions.questions[2];
+  
+  console.log("First question:", question1);
   // then we render the page
-  res.render('ladySprout', { question: firstQuestion });
+  res.render('ladySprout', { question: question1 });
 });
 
 module.exports = router;
