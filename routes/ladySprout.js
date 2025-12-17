ff1eb5fe85ea4f08b46bbdc7614894e54c251131
juliaczernek <czernek.julka@gmail.com>
2025-12-17 11:47:03 +0100

    pani sprouts została zmieniona - wszystko działa! :)

--- routes/ladySprout.js
+++ routes/ladySprout.js
@@ -2,19 +2,36 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 
+const data = fs.readFileSync('public/databases/data-ladySprout.json', 'utf8');
+const allQuestions = JSON.parse(data);
 
 // get question
-router.get('/', function(req, res, next) {
-  // first we get the data
-  const data = fs.readFileSync('public/databases/ladySprout.json', 'utf8');
-  const questions = JSON.parse(data);
-  const question1 = questions.questions[1]; 
-  const question2 = questions.questions[1]; 
-  const question3 = questions.questions[2];
+router.get('/', function(req, res) {
+  const questionId = req.query.question;
+  const answerText = req.query.answer;
+ 
+  // if no parameter show first question
+  if(!questionId){
+    const firstQuestion = allQuestions.questions.find(q => q.question_id ===1);
+    return res.render('ladySprout',{question:firstQuestion});
+  }
+
+// find current question
+  const question = allQuestions.questions.find(q => q.question_id == questionId);
+
+  const picked = question.answers.find(a => a.answer === answerText);
   
-  console.log("First question:", question1);
-  // then we render the page
-  res.render('ladySprout', { question: question1 });
+  if (picked.encyclopedia_id){
+    return res.redirect(`/encyclopedia/${picked.encyclopedia_id}`);
+  }
+  
+  if (picked.id_of_next_question){
+    const nextQuestion = allQuestions.questions.find(
+      q=> q.question_id === picked.id_of_next_question);
+      return res.render('ladySprout', {question: nextQuestion});
+  }
+  
+
 });
 
 module.exports = router;
